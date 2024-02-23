408434fefbdb43b7630ef5dc860a58d22aeb7d06
PrashantM512 <prashantmorade512@gmail.com>
2024-02-23 11:46:55 +0530

    Initial commit

---
+++ src/main/java/com/eazylearn/EazyLearnApplication.java
@@ -0,0 +1,13 @@
+package com.eazylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eazylearn/EazyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
