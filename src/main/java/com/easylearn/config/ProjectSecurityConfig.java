837ad8bdd723880e50f69d6aa9a760905df38795
PrashantM512 <prashantmorade512@gmail.com>
2024-02-26 11:58:49 +0530

    CSRF security added to the login page

--- src/main/java/com/easylearn/config/ProjectSecurityConfig.java
+++ src/main/java/com/easylearn/config/ProjectSecurityConfig.java
@@ -15,29 +15,44 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectSecurityConfig {
 
-  @Autowired
-  private UserDetailsService userDetailsService;
-
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf((csrf) -> csrf.disable())
+	  http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg"))
       .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
-      .requestMatchers("", "/", "/home").permitAll()
-      .requestMatchers("/holidays/**").permitAll()
-      .requestMatchers("/contact").permitAll()
-      .requestMatchers("/saveMsg").permitAll()
-      .requestMatchers("/courses").permitAll()
-      .requestMatchers("/about").permitAll()
-      .requestMatchers("/login").permitAll()
-      .requestMatchers("/assets/**").permitAll())
+          .requestMatchers("/displayMessages").hasRole("ADMIN")
+          .requestMatchers("/closeMsg/**").hasRole("ADMIN")
+          .requestMatchers("", "/", "/home").permitAll()
+          .requestMatchers("/holidays/**").permitAll()
+          .requestMatchers("/contact").permitAll()
+          .requestMatchers("/saveMsg").permitAll()
+          .requestMatchers("/courses").permitAll()
+          .requestMatchers("/about").permitAll()
+          .requestMatchers("/assets/**").permitAll()
+          .requestMatchers("/login").permitAll()
+          .requestMatchers("/logout").permitAll())
       .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
-        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+              .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
       .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
-        .invalidateHttpSession(true).permitAll())
-      .httpBasic(Customizer.withDefaults())
-      .userDetailsService(userDetailsService); // add this line
+              .invalidateHttpSession(true).permitAll())
+      .httpBasic(Customizer.withDefaults());
      
     return http.build();
+  }
+  
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+      UserDetails user = User.withDefaultPasswordEncoder()
+              .username("user")
+              .password("12345")
+              .roles("USER")
+              .build();
+      UserDetails admin = User.withDefaultPasswordEncoder()
+              .username("admin")
+              .password("54321")
+              .roles("ADMIN")
+              .build();
+      return new InMemoryUserDetailsManager(user, admin);
   }
 
 }
